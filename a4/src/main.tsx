90a520d90a83d3df8c2ee906104c295a0bb48b21
Kevin Gu <132306047+kevingu8@users.noreply.github.com>
2025-07-23 22:43:58 -0400

    3

--- a4/src/main.tsx
+++ a4/src/main.tsx
@@ -1,6 +1,7 @@
 import { render } from "preact";
 import { Model } from "./model";
 import { MainView } from "./views/mainView";
+import "./app.css";
 
 const model = new Model();
 const root = document.querySelector("div#app")!;
